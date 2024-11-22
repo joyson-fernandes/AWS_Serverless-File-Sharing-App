86af27b540872293bfe6cca24967ea0a5e94b903
Joyson Fernandes <jf@joysontech.com>
2024-11-22 10:50:27 +0000

    Update DownloadFunction.py

--- DownloadFunction.py
+++ DownloadFunction.py
@@ -3,14 +3,11 @@ import boto3
 import base64
 
 s3 = boto3.client('s3')
-BUCKET_NAME = 'my-file-sharing-bucket-amc'
+BUCKET_NAME = 'joy-file-sharing-bucket'
 
 def lambda_handler(event, context):
     try:
-        # Get the file name from the query string parameters
         file_name = event['queryStringParameters']['fileName']
-        
-        # Retrieve the file from S3
         file_obj = s3.get_object(Bucket=BUCKET_NAME, Key=file_name)
         file_content = file_obj['Body'].read()
         
